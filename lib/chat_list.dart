6fa16b5e74afc7f295293b7a0cd263fe8f657608
Bashar Awada <baa64@mail.aub.edu>
2020-03-23 18:40:28 +0200

    updated

--- lib/chat_list.dart
+++ lib/chat_list.dart
@@ -1,25 +1,261 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:contacts_service/contacts_service.dart';
+import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'chat_room.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
+class ChatList extends StatefulWidget {
+  @override
+  _ChatListState createState() => _ChatListState();
+}
 
-class ChatList extends StatelessWidget {
+class _ChatListState extends State<ChatList> {
+  Iterable<Contact> _contacts;
+  HashMap<int, String> usermap;
+  QuerySnapshot users;
+  getdata() async {
+    QuerySnapshot users =
+        await Firestore.instance.collection('users').getDocuments();
+    return users;
+  }
+
+  HashMap<int, String> createmap() {
+    HashMap<int, String> usersmap = new HashMap<int, String>();
+    for (int i = 0; i < users.documents.length; i++) {
+      usersmap[i] = users.documents[i].data['Phone'];
+     // print("aa");
+    }
+    return usersmap;
+  }
+
+  @override
+  initState() {
+    getdata().then((results) {
+      setState(() {
+        users = results;
+       // print("a");
+      });
+      usermap = createmap();
+      //print(usermap);
+    });
+
+    super.initState();
+    refreshContacts();
+  }
+
+  refreshContacts() async {
+    PermissionStatus permissionStatus = await _getContactPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      var contacts = await ContactsService.getContacts();
+//      var contacts = await ContactsService.getContactsForPhone("8554964652");
+      setState(() {
+        _contacts = contacts;
+      });
+    } else {
+      _handleInvalidPermissions(permissionStatus);
+    }
+  }
+
+  updateContact() async {
+    Contact ninja = _contacts
+        .toList()
+        .firstWhere((contact) => contact.familyName.startsWith("Ninja"));
+    ninja.avatar = null;
+    await ContactsService.updateContact(ninja);
+
+    refreshContacts();
+  }
+
+  Future<PermissionStatus> _getContactPermission() async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.contacts);
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.restricted) {
+      Map<PermissionGroup, PermissionStatus> permissionStatus =
+          await PermissionHandler()
+              .requestPermissions([PermissionGroup.contacts]);
+      return permissionStatus[PermissionGroup.contacts] ??
+          PermissionStatus.unknown;
+    } else {
+      return permission;
+    }
+  }
+
+  void _handleInvalidPermissions(PermissionStatus permissionStatus) {
+    if (permissionStatus == PermissionStatus.denied) {
+      throw new PlatformException(
+          code: "PERMISSION_DENIED",
+          message: "Access to location data denied",
+          details: null);
+    } else if (permissionStatus == PermissionStatus.restricted) {
+      throw new PlatformException(
+          code: "PERMISSION_DISABLED",
+          message: "Location data is not available on device",
+          details: null);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return ListTile(
-        leading: CircleAvatar(
-                  radius: 23,
-                  child:Icon(Icons.person)),
-        title: Text('Contact $index'),
-        //trailing: Text(DateTime.now().toString()),
-        //// onTap START ////
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return ChatRoom(); // passing data to chat room
-          }));
-        },
-        //// onTap END ////
-      );
-    });
+    List<Contact> ali=[];
+    List a=[];
+    for(int i=0;i<_contacts.length;i++)
+    {
+      var numb;
+      _contacts.elementAt(i).phones.map((f) => numb=(f.value.trim())??" ").toList();
+      
+      numb=numb.toString().replaceAll(new RegExp(r"\s+\b|\b\s"), "");
+      if(!numb.contains("+961")){
+        numb="+961" + numb;
+      }
+      print(numb);
+       if (usermap.containsValue(numb)){
+                    //print((c.phones?.elementAt(i).toString().trim()));
+                    if(!(a.contains(numb))){
+                    ali.add(_contacts.elementAt(i));
+                    a.add(numb);
+                    }
+                   
+                    // break;
+                  }
+                
+
+    }
+    
+    return SafeArea(
+      child: _contacts != null
+          ? ListView.builder(
+              itemCount: ali.length,
+              itemBuilder: (BuildContext context, int index) {
+                Contact c = ali?.elementAt(index);
+                
+                return ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) =>ChatRoom()));
+                  },
+                  leading: (c.avatar != null && c.avatar.length > 0)
+                      ? CircleAvatar(backgroundImage: MemoryImage(c.avatar))
+                      : CircleAvatar(child: Text(c.initials())),
+                  title: Text(c.displayName ?? ""),
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
+
+class ContactDetailsPage extends StatelessWidget {
+  ContactDetailsPage(this._contact);
+  final Contact _contact;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_contact.displayName ?? ""),
+        actions: <Widget>[
+//          IconButton(
+//            icon: Icon(Icons.share),
+//            onPressed: () => shareVCFCard(context, contact: _contact),
+//          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () => ContactsService.deleteContact(_contact),
+          ),
+          IconButton(icon: Icon(Icons.update), onPressed: () {}),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Name"),
+              trailing: Text(_contact.givenName ?? ""),
+            ),
+            ListTile(
+              title: Text("Middle name"),
+              trailing: Text(_contact.middleName ?? ""),
+            ),
+            ListTile(
+              title: Text("Family name"),
+              trailing: Text(_contact.familyName ?? ""),
+            ),
+            ListTile(
+              title: Text("Prefix"),
+              trailing: Text(_contact.prefix ?? ""),
+            ),
+            ListTile(
+              title: Text("Suffix"),
+              trailing: Text(_contact.suffix ?? ""),
+            ),
+            ListTile(
+              title: Text("Birthday"),
+              trailing: Text(_contact.birthday != null
+                  ? DateFormat('dd-MM-yyyy').format(_contact.birthday)
+                  : ""),
+            ),
+            ListTile(
+              title: Text("Company"),
+              trailing: Text(_contact.company ?? ""),
+            ),
+            ListTile(
+              title: Text("Job"),
+              trailing: Text(_contact.jobTitle ?? ""),
+            ),
+            ListTile(
+              title: Text("Account Type"),
+              trailing: Text((_contact.androidAccountType != null)
+                  ? _contact.androidAccountType.toString()
+                  : ""),
+            ),
+            //AddressesTile(_contact.postalAddresses),
+            ItemsTile("Phones", _contact.phones),
+            ItemsTile("Emails", _contact.emails)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemsTile extends StatelessWidget {
+  ItemsTile(this._title, this._items);
+  final Iterable<Item> _items;
+  final String _title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        ListTile(title: Text(_title)),
+        Column(
+          children: _items
+              .map(
+                (i) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: ListTile(
+                    title: Text(i.label ?? ""),
+                    trailing: Text(i.value ?? ""),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ],
+    );
   }
 }
--- lib/screens/SignIn.dart
+++ lib/screens/SignIn.dart
@@ -1,93 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 import 'home/home.dart';
 
 class LoginScreen extends StatelessWidget {
   final _phoneController = TextEditingController();
   final _codeController = TextEditingController();
 
-  Future<bool> loginUser(String phone, BuildContext context) async{
+  Future<bool> loginUser(String phone, BuildContext context) async {
     FirebaseAuth _auth = FirebaseAuth.instance;
 
     _auth.verifyPhoneNumber(
         phoneNumber: phone,
         timeout: Duration(seconds: 60),
-        verificationCompleted: (AuthCredential credential) async{
+        verificationCompleted: (AuthCredential credential) async {
           Navigator.of(context).pop();
 
           AuthResult result = await _auth.signInWithCredential(credential);
 
           FirebaseUser user = result.user;
 
-          if(user != null){
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) => Home()
-            ));
-          }else{
+          if (user != null) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Home()));
+          } else {
             print("Error");
           }
 
           //This callback would gets called when verification is done auto maticlly
         },
-        verificationFailed: (AuthException exception){
+        verificationFailed: (AuthException exception) {
           print(exception);
         },
-        codeSent: (String verificationId, [int forceResendingToken]){
+        codeSent: (String verificationId, [int forceResendingToken]) {
           showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("Give the code?"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextField(
-                      controller: _codeController,
-                    ),
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Give the code?"),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextField(
+                        controller: _codeController,
+                      ),
+                    ],
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("Confirm"),
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      onPressed: () async {
+                        final code = _codeController.text.trim();
+                        AuthCredential credential =
+                            PhoneAuthProvider.getCredential(
+                                verificationId: verificationId, smsCode: code);
+
+                        AuthResult result =
+                            await _auth.signInWithCredential(credential);
+
+                        FirebaseUser user = result.user;
+
+                        if (user != null) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => Home()));
+                        } else {
+                          print("Error");
+                        }
+                      },
+                    )
                   ],
-                ),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text("Confirm"),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () async{
-                      final code = _codeController.text.trim();
-                      AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
-
-                      AuthResult result = await _auth.signInWithCredential(credential);
-
-                      FirebaseUser user = result.user;
-
-                      if(user != null){
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) => Home()
-                        ));
-                      }else{
-                        print("Error");
-                      }
-                    },
-                  )
-                ],
-              );
-            }
-          );
+                );
+              });
         },
-        codeAutoRetrievalTimeout: null
-    );
+        codeAutoRetrievalTimeout: null);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Color(0xff075e54),
-        elevation: 0.0,
-        title: Text('Sign in to Whatsup'),
-      ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Color(0xff075e54),
+          elevation: 0.0,
+          title: Text('Sign in to Whatsup'),
+        ),
         body: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.all(32),
@@ -96,31 +96,32 @@ class LoginScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("Sign in with phone number", style: TextStyle(color: Color(0xff075e54), fontSize: 16, fontWeight: FontWeight.w500),),
-
-                  SizedBox(height: 16,),
-
+                  Text(
+                    "Sign in with phone number",
+                    style: TextStyle(
+                        color: Color(0xff075e54),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
                   TextFormField(
                     decoration: InputDecoration(
                         enabledBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(8)),
-                            borderSide: BorderSide(color: Colors.grey[200])
-                        ),
+                            borderSide: BorderSide(color: Colors.grey[200])),
                         focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(8)),
-                            borderSide: BorderSide(color: Colors.grey[300])
-                        ),
+                            borderSide: BorderSide(color: Colors.grey[300])),
                         filled: true,
                         fillColor: Colors.grey[100],
-                        hintText: "Mobile Number"
-
-                    ),
+                        hintText: "Mobile Number"),
                     controller: _phoneController,
                   ),
-
-                  SizedBox(height: 16,),
-
-
+                  SizedBox(
+                    height: 16,
+                  ),
                   Container(
                     width: double.infinity,
                     child: FlatButton(
@@ -128,18 +129,37 @@ class LoginScreen extends StatelessWidget {
                       textColor: Colors.white,
                       padding: EdgeInsets.all(16),
                       onPressed: () async {
+                        QuerySnapshot users = await Firestore.instance
+                            .collection('users')
+                            .getDocuments();
                         showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Center(child: CircularProgressIndicator(),);
-                  });
-                  await loginAction();
-                  final phone = _phoneController.text.trim();
+                            context: context,
+                            builder: (BuildContext context) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            });
+                        await loginAction();
+                        final phone = _phoneController.text.trim();
 
                         loginUser(phone, context);
-
-                  Navigator.pop(context);
-                        
+                        bool found = false;
+                        for (int i = 0; i < users.documents.length; i++) {
+                          if (users.documents[i].data['Phone'] == phone) {
+                            found = true;
+                            break;
+                          }
+                        }
+                        if (FirebaseAuth.instance.currentUser() != null && found!=true) {
+                          Firestore.instance.collection('users').add({
+                            'Phone': phone,
+                          }).catchError((e) {
+                            print(e);
+                          });
+                        } else {
+                          print('You need to be logged in');
+                        }
+                        Navigator.pop(context);
                       },
                       color: Color(0xff075e54),
                     ),
@@ -148,9 +168,9 @@ class LoginScreen extends StatelessWidget {
               ),
             ),
           ),
-        )
-    );
+        ));
   }
+
   Future<bool> loginAction() async {
     //replace the below line of code with your login request
     await new Future.delayed(const Duration(seconds: 2));
